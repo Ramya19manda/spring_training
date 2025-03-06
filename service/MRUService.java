51049716edc974cc03ca75b78ef14d0c272f6d2f
Ramya19manda <158134173+Ramya19manda@users.noreply.github.com>
2025-03-06 12:15:02 +0530

    Add files via upload

---
+++ service/MRUService.java
@@ -0,0 +1,22 @@
+package com.mru.service;
+
+import java.util.List;
+
+import org.hibernate.boot.model.internal.ListBinder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mru.entity.Laptops;
+import com.mru.repo.MRURepo;
+
+@Service
+public class MRUService {
+	@Autowired
+	private MRURepo repo;
+	public List<Laptops> getAllLaptops(){
+		return repo.findAll();
+	}
+	
+	
+
+}
